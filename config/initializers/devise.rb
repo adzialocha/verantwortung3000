fb806d15076630c17f0d75dd5e0982150ad24c03
Andreas Dzialocha <andreas@marmorkuchen.net>
2015-08-16 16:49:52 +0200

    clean up

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'kontakt@blatt3000.de'
+  config.mailer_sender = 'BLATT 3000 <kontakt@blatt3000.de>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
