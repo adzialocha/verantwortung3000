060837f042847985e2cca908d28a7175af5c23c5
Andreas Dzialocha <andreas@marmorkuchen.net>
2015-08-22 16:29:44 +0200

    language switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
 
+  before_action :set_locale
+
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
+  end
+
+  private
+
+  def set_locale
+
+    if user_signed_in? and current_user.locale.present?
+      session[:locale] = current_user.locale
+    elsif I18n.available_locales.map(&:to_s).include?(params[:locale])
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Darmstadt3000
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+    config.i18n.available_locales = [:de, :en]
+    config.i18n.enforce_available_locales = false
 
     config.active_record.raise_in_transactional_callbacks = true
 
