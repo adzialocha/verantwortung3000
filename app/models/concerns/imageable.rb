5e4ee6d36a7e5642118a175b1ca5970f72c26b02
Andreas Dzialocha <andreas@marmorkuchen.net>
2015-08-22 23:33:35 +0200

    correct destroy action

--- app/models/concerns/imageable.rb
+++ app/models/concerns/imageable.rb
@@ -4,7 +4,7 @@ module Imageable
 
   included do
 
-    has_many :images, as: :imageable, dependent: :delete_all
+    has_many :images, as: :imageable, dependent: :destroy
     accepts_nested_attributes_for :images, reject_if: :all_blank, allow_destroy: true
 
   end
