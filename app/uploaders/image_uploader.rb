4da7329b14852c0416177e6f36f8a5d1612b83da
Andreas Dzialocha <andreas@marmorkuchen.net>
2015-09-07 02:45:21 +0200

    correct quotes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,11 +7,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :fog
 
   def store_dir
-    'uploads/#{model.class.to_s.underscore}/#{model.id}'
+    "uploads/#{model.class.to_s.underscore}/#{model.id}"
   end
 
   def default_url(*args)
-    '/images/default_image.jpg'
+    "/images/default_image.jpg"
   end
 
   process :resize_to_fit => [960, 960]
