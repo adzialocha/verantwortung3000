895fc905f50bfc498f6017caf96002f99607fd8e
Andreas Dzialocha <andreas@marmorkuchen.net>
2015-09-25 01:15:30 +0200

    minor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {
-    :host => "verantwortung3000.de",
+    :host => "http://www.verantwortung3000.de",
     :port => 80
   }
 
