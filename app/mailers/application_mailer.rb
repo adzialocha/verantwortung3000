4f0c2bfb3439d60080697bb028c434d0772a9d13
Andreas Dzialocha <andreas@marmorkuchen.net>
2015-09-29 01:23:49 +0200

    change mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ class ApplicationMailer < ActionMailer::Base
 
   add_template_helper ApplicationHelper
 
-  default from: "kontakt@blatt3000.de"
+  default from: "VERANTWORTUNG 3000 <kontakt@verantwortung3000.de>"
   layout 'mailer'
 
 end
