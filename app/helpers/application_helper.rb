928f1b7be4ce9708e492fb229bd1280c2ad4c8bd
Andreas Dzialocha <andreas@marmorkuchen.net>
2015-10-01 01:36:53 +0200

    minor bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
       'Ciao',
       'Beep Beep',
       'Bis bald!',
-      'Machs gut!'
+      'Machs gut!',
       'Tschö mit ö!',
       'Hau rein!',
       'Tschüssi!',
